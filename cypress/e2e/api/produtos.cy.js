a5d6070ccc92df12643f9214a479d944bc590d11
Ricardo Montanari <montanari.rc@gmail.com>
2025-01-09 07:48:30 -0300

    inclusão de testes de api e alterações em configurações

---
+++ cypress/e2e/api/produtos.cy.js
@@ -0,0 +1,20 @@
+describe('Testes do endpoint /usuarios', () => {
+
+    const API_URL = Cypress.env('API_BASE_URL')
+    const id_produto = Cypress.env('ID_PRODUTO')
+
+    it.only('Validar a busca de um produto pelo id', () => {
+        cy.request({
+            method: 'GET',
+            url: `${API_URL}/produtos/${id_produto}`
+        }).should(({ status, body }) => {
+            const { nome, preco, descricao, quantidade } = body
+            
+            expect(status).to.eq(200)
+            expect(nome).to.eq('TekPix 1736284288067')
+            expect(preco).to.eq(580)
+            expect(descricao).to.eq('Tekpix 2008')
+            expect(quantidade).to.eq(5)
+        })
+    })
+})
---
+++ cypress/e2e/api/usuarios.cy.js
@@ -0,0 +1,39 @@
+describe('Testes do endpoint /usuarios', () => {
+
+    const API_URL = Cypress.env('API_BASE_URL')
+    const id_user = Cypress.env('ID_USER')
+
+    it('Validar a busca dos produtos pelo id', () => {
+        cy.request({
+            method: 'GET',
+            url: `${API_URL}/usuarios/${id_user}`
+        }).should(({ status, body }) => {
+            const { nome, email, password, administrador } = body
+            
+            expect(status).to.eq(200)
+            expect(nome).to.eq('Ricardo Montanari')
+            expect(email).to.eq('montanari.rc@gmail.com')
+            expect(password).to.eq('teste123')
+            expect(administrador).to.eq('true')
+        })
+    })
+
+    it('Validar que um usuário não pode ser cadastrado com um email existente', () => {
+        cy.request({
+            method: 'POST',
+            url: `${API_URL}/usuarios`,
+            body: {
+                "nome": "Fulano da Silva",
+                "email": "montanari.rc@gmail.com",
+                "password": "teste",
+                "administrador": "true"
+              },
+              failOnStatusCode: false
+        }).should(({ body }) => {
+            const { status, message } = body
+
+            expect(message).to.eq('Este email já está sendo usado')
+        })
+    })
+
+})
